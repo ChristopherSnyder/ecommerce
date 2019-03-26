851c17ca1ef6cdd17eb4ee1db0999b62f988b77c
Chris Snyder <christopher.snyder@aceproject.space>
2019-03-26 12:20:58 -0500

    Login now checks database before logging you in, as one should do

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,23 @@ class UsersController < ApplicationController
         if request.post?
             @givenuser =  params[:user][:user]
             @givenpass = params[:pass][:pass]
-            redirect_to "/"
-         end
+
+
+            @user = User.where('username = ?', @givenuser).first
+            if @user
+                if @user.password = @givenpass
+                session[:current_user_name] = @user.username
+                redirect_to "/"
+                end
+
+            else
+                redirect_to "/login/", notice: "Invalid user or password"
+            end
         end
+    end
+
+
+
 
 
     def logout
