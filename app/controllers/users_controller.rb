ab91ddbfe31697c7a113a388a901004064d72971
Chris Snyder <christopher.snyder@aceproject.space>
2019-03-26 19:36:46 -0500

    You can now register a new user-access account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
             if @user
                 if @user.password = @givenpass
                 session[:current_user_name] = @user.username
+                session[:current_user_id] = @user.id
+                session[:current_user_level] = @user.accesslevel
+                session[:current_user_province] = @user.province
+
                 redirect_to "/"
                 end
 
@@ -39,6 +43,36 @@ class UsersController < ApplicationController
 
     def register
         @province = Province.all
+
+    end
+
+
+    def processregister
+
+        @givenuser = params[:user][:user]
+        @givenpass = params[:pass][:pass]
+        @givencity = params[:city][:city]
+
+        @givenprovince = params[:post][:province]
+        @givenprovince = Province.find(@givenprovince)
+        @givenprovince = @givenprovince.name
+
+
+        @newuser = User.new
+        @newuser.username = @givenuser
+        @newuser.password = @givenpass
+        @newuser.city = @givencity
+        @newuser.province = @givenprovince
+        @newuser.accesslevel = 1
+        @newuser.save
+
+        session[:current_user_name] = @newuser.username
+        session[:current_user_id] = @newuser.id
+        session[:current_user_level] = @newuser.accesslevel
+        session[:current_user_province] = @newuser.province
+
+        redirect_to "/"
+
 
     end
 
