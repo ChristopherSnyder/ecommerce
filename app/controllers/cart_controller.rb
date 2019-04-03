c7471dfe0eb0d8fcdfd756273f60152417de83a9
Chris Snyder <christopher.snyder@aceproject.space>
2019-04-03 11:46:48 -0500

    You can now decrease quantity

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,11 @@
 class CartController < ApplicationController
     def lowerquantity
 
+        @item = Cart.where('id = ?', params[:id]).first
+        @item.quantity -= 1
+        @item.save
+
+        redirect_to "/mycart"
+
     end
 end
