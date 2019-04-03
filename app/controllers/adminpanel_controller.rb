047323e61d9dffaf003043ef4b8923ab01c707c9
Chris Snyder <christopher.snyder@aceproject.space>
2019-04-03 13:56:48 -0500

    Admins can now view all orders that need shipping

--- app/controllers/adminpanel_controller.rb
+++ app/controllers/adminpanel_controller.rb
@@ -2,5 +2,11 @@ class AdminpanelController < ApplicationController
 
 
     def index
+        @needshipping = Order.where(shipped: [false, nil])
+        @pastorders = Order.all
+
+
+
+
     end
 end
